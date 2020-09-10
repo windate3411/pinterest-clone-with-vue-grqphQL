0023b03644bb980909bc14fd346012b7a38132af
windate3411 <windate3411@gmail.com>
2020-09-10 15:42:18 +0800

    create Post schema

---
+++ models/Post.js
@@ -0,0 +1,52 @@
+const moogoose = require('mongoose')
+
+const PostSchema = new moogoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  imgUrl: {
+    type: String,
+    required: true,
+  },
+  categories: {
+    type: [String],
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  createdBy: {
+    type: moogoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  messages: [
+    {
+      messageBody: {
+        type: String,
+        required: true,
+      },
+      messageDate: {
+        type: Date,
+        default: Date.now,
+      },
+      messageUser: {
+        type: moogoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+    },
+  ],
+})
+
+module.exports = moogoose.model('Post', PostSchema)
