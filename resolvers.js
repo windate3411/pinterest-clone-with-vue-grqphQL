7ec83ebd0987ce6963f8b46967bda6864592a5e1
windate3411 <windate3411@gmail.com>
2020-09-11 10:35:31 +0800

    create addPost mutation

--- resolvers.js
+++ resolvers.js
@@ -10,5 +10,19 @@ module.exports = {
       }).save()
       return newUser
     },
+    addPost: async (
+      _,
+      { title, description, categories, imgUrl, creatorId },
+      { Post }
+    ) => {
+      const newPost = await new Post({
+        title,
+        description,
+        categories,
+        imgUrl,
+        createdBy: creatorId,
+      }).save()
+      return newPost
+    },
   },
 }
