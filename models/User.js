ce38ea478d501e6da38d0156acb333712a5e8f27
windate3411 <windate3411@gmail.com>
2020-09-12 11:41:55 +0800

    add preset user avatar and hash the password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const moogoose = require('mongoose')
+const md5 = require('md5')
+const bcrypt = require('bcrypt')
 
 const UserSchema = new moogoose.Schema({
   username: {
@@ -28,6 +30,25 @@ const UserSchema = new moogoose.Schema({
     required: true,
     ref: 'Post',
   },
+})
+
+// create user default avatar before saving
+UserSchema.pre('save', function (next) {
+  this.avatar = `https://gravatar.com/avatar/${md5(this.username)}?d=identicon`
+  next()
+})
+
+// hash password
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next()
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err)
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err)
+      this.password = hash
+      next()
+    })
+  })
 })
 
 module.exports = moogoose.model('User', UserSchema)
