1ed0690558a401c33b7d33c2b55e6dc63f523ae1
windate3411 <windate3411@gmail.com>
2020-09-26 19:45:37 +0800

    add userPosts to Profile.vue

--- client/src/components/Auth/Profile.vue
+++ client/src/components/Auth/Profile.vue
@@ -1,7 +1,31 @@
 <template>
   <v-container>
     <UserProfileCard :user="currentUser" />
-    <PostCard :posts="currentUser.favorites" />
+    <!-- tabs -->
+    <v-tabs v-model="tab" icon-and-text>
+      <v-tabs-slider></v-tabs-slider>
+      <v-tab href="#favorites">
+        Favorites
+        <v-icon class="ml-2">mdi-heart</v-icon>
+      </v-tab>
+      <v-tab href="#userPosts">
+        Your Posts
+        <v-icon class="ml-2">mdi-chat</v-icon>
+      </v-tab>
+    </v-tabs>
+
+    <!-- display posts -->
+    <PostCard :posts="renderPosts" />
+
+    <!-- observer -->
+    <div class="d-flex justify-center">
+      <v-progress-circular
+        indeterminate
+        color="primary"
+        v-intersect="onIntersect"
+        v-if="hasMore"
+      ></v-progress-circular>
+    </div>
   </v-container>
 </template>
 
@@ -16,17 +40,42 @@ export default {
     UserProfileCard,
     PostCard
   },
+  data() {
+    return {
+      isIntersecting: false,
+      pageNum: 1,
+      numsPerPage: 2,
+      tab: ''
+    }
+  },
   computed: {
-    ...mapGetters(['currentUser', 'userPosts'])
+    ...mapGetters(['currentUser', 'userPosts']),
+    renderPosts() {
+      const posts = this.tab === 'userPosts'? this.userPosts : this.currentUser.favorites
+      return posts.slice(0,this.pageNum * this.numsPerPage)
+    },
+    hasMore() {
+      const posts = this.tab === 'userPosts'? this.userPosts : this.currentUser.favorites
+      return posts.length !== this.renderPosts.length
+    }
   },
   methods: {
     ...mapActions(['getUserPosts']),
     handleGetUserPosts() {
-      console.log(this.currentUser._id)
       this.getUserPosts({
         user_id:this.currentUser._id
       })
+    },
+    onIntersect (entries, observer) {
+        this.isIntersecting = entries[0].isIntersecting
+        if (entries[0].isIntersecting) {
+          this.handleScroll()
+        }
+    },
+    handleScroll() {
+      this.pageNum++
     }
+
   },
   created() {
     this.handleGetUserPosts()
